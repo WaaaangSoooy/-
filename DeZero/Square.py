3711a60ea6a762c6396e913242e195e4beca1692
wangsong <wangsongyan1@outlook.com>
2026-05-15 23:39:55 +0800

    stagetwo

--- DeZero/Square.py
+++
@@ -1,9 +0,0 @@
-from DeZero.Function import Function
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
